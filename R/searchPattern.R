30ca1c0a0da304bd88d30f83d603d583ce5667f9
Lihua Julie Zhu <julie.zhu@umassmed.edu>
2012-03-07 01:32:44 +0000

    added cut frequency summary for hg19 TSS 1kb and cut pattern for allrestriction enzymes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/REDseq@63562 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/searchPattern.R
+++ R/searchPattern.R
@@ -1,46 +1,46 @@
-searchPattern <-
-function(dict0, BSgenomeName, outfile="")
- { 	
- 	
-		if(missing(dict0) || class(dict0) != "DNAStringSet" )
-		{
-			stop("REpatternFilePath is required as a DNAstringSet object!")
-		}
-		if (missing(BSgenomeName) || class(BSgenomeName) != "BSgenome")
+searchPattern <-function (dict0,  BSgenomeName, outfile = "") 
+{
+    if (missing(dict0) || class(dict0) != "DNAStringSet") {
+        stop("dictO is required as a DNAStringSet object!")
+    }
+	if (missing(BSgenomeName) || class(BSgenomeName) != "BSgenome")
 		{
 			stop("BSgenomeName is required as a BSgenome object!")
 		}
-		if (file.exists(outfile))
-		{
-			stop("outfile specified as ", outfile, " already exists! Please rename the outfile!")
+    if (file.exists(outfile)) {
+        stop("outfile specified as ", outfile, " already exists! Please rename the outfile!")
+    }
+    seqnames <- seqnames(BSgenomeName)
+    seqnames_in1string <- paste(seqnames, collapse = ", ")
+    append <- FALSE
+    for (seqname in seqnames) {
+        subject <- BSgenomeName[[seqname]]
+        cat(">>> Finding all hits in sequences", seqname, "...\n")
+        for (i in seq_len(length(dict0))) {
+            patternID <- names(dict0)[i]
+            if (length(patternID) < 1) {
+                patternID = paste("pattern", i, sep = "")
+            }
+            pattern <- dict0[[i]]
+            plus_matches <- matchPattern(pattern, subject, fixed = FALSE)
+	    if (length(plus_matches)>0)
+	   {
+		 names(plus_matches) <- rep.int(paste(patternID, "+",sep="."), length(plus_matches))
+           	 writeHits(seqname, plus_matches, "+", file = outfile, append = append)
+            	append <- TRUE
+	  }
+            rcpattern <- reverseComplement(pattern)
+            if (rcpattern != pattern) {
+                minus_matches <- matchPattern(rcpattern, subject,fixed = FALSE)
+		 if (length(minus_matches)>0)
+           	{
+                	names(minus_matches) <- rep.int(paste(patternID,"-", sep="."), length(minus_matches))
+                	writeHits(seqname, minus_matches, "-", file = outfile, 	append = append)
+			append <- TRUE
 		}
-
-	seqnames <- seqnames(BSgenomeName)
-	seqnames_in1string <- paste(seqnames, collapse=", ")
-  append <- FALSE
- for (seqname in seqnames) {
-		subject <- BSgenomeName[[seqname]]
- 		cat(">>> Finding all hits in chromosome", seqname, "...\n")
- 		for (i in seq_len(length(dict0))) {
- 			patternID <- names(dict0)[i]
-			if (length(patternID)<1)
-			{
-				patternID=paste("pattern", i,sep="")
-			}
- 			pattern <- dict0[[i]]
- 			plus_matches <- matchPattern(pattern, subject, fixed=FALSE)
- 			names(plus_matches) <- rep.int(patternID, length(plus_matches))
- 			writeHits(seqname, plus_matches, "+", file=outfile, append=append)
- 			append <- TRUE
- 			rcpattern <- reverseComplement(pattern)
-			if (rcpattern != pattern)
-			{
- 				minus_matches <- matchPattern(rcpattern, subject, fixed=FALSE)
- 				names(minus_matches) <- rep.int(patternID, length(minus_matches))
- 				writeHits(seqname, minus_matches, "-", file=outfile, append=append)
-			}
- 		}
- 	cat(">>> DONE searching\n")
- 	}
- }
+            }
+        }
+        cat(">>> DONE searching\n")
+    }
+}
 
